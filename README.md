66fbce8434dc6186600921a67c79ea4bbb88f3db
tsayles <TSayles@Soot-n-Smoke.com>
2023-10-07 12:02:32 -0700

    Add scraper script and documentation
    - Added `scraper.py`: A script to scrape property listings from Zillow, Redfin, and One Home and update the data in a Google Sheet.
    - Added `README.md`: Provides a description of the script's functionality, usage instructions, and attribution.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Listing Scraper
+
+This Python script automates the process of scraping property listings from Zillow, Redfin, and One Home. The scraped data, which includes the property's status, asking price, square footage, and address, is then updated in a specified Google Sheet.
+
+
+## Attribution
+
+- **Author**: Tom Sayles
+- **Contributor**: ChatGPT-4.0 by OpenAI
+
+## Usage
+
+1. Set up the Google Sheets API and obtain `path_to_credentials.json` for authentication.
+2. Install the required Python libraries:
+3. Run the script:
---
+++ scraper.py
@@ -0,0 +1,51 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+from bs4 import BeautifulSoup
+
+def get_links_from_sheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name('path_to_credentials.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1NdMsw3amImspKbzL25Ay6Xf1JWyPlL9PvMAh2u6eTfQ/edit?usp=sharing").sheet1
+    links = sheet.col_values(1)
+    return sheet, links[1:]
+
+def scrape_data(link):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(link, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    if "zillow.com" in link:
+        status = soup.find('span', class_='status').text
+        asking = soup.find('span', class_='price').text
+        sqft = soup.find('span', class_='sqft').text
+        address = soup.find('span', class_='address').text
+    elif "redfin.com" in link:
+        status = soup.find('span', class_='status').text
+        asking = soup.find('span', class_='price').text
+        sqft = soup.find('span', class_='sqft').text
+        address = soup.find('span', class_='address').text
+    elif "onehome.com" in link:
+        status = soup.find('span', class_='status').text
+        asking = soup.find('span', class_='price').text
+        sqft = soup.find('span', class_='sqft').text
+        address = soup.find('span', class_='address').text
+    else:
+        return None
+    return status, asking, sqft, address
+
+def update_sheet(sheet, row, data):
+    for i, value in enumerate(data, start=2):
+        sheet.update_cell(row, i, value)
+
+def main():
+    sheet, links = get_links_from_sheet()
+    for idx, link in enumerate(links, start=2):
+        data = scrape_data(link)
+        if data:
+            update_sheet(sheet, idx, data)
+
+if __name__ == "__main__":
+    main()
